46c55caa1918dc5572236f713654f224cb2a632c
shouya <526598+shouya@users.noreply.github.com>
2024-07-16 18:24:10 +0900

    project 03: (probably) correctly stripify the mesh

--- project-03-shading/src/mesh.rs
+++ project-03-shading/src/mesh.rs
@@ -215,40 +215,23 @@ impl From<&TriangleIndex> for TriangleStrip {
     }: &TriangleIndex,
   ) -> Self {
     let mut ranges = Vec::new();
-    let mut total_trigs = 0;
-    let expected_total_trigs = indices.len() / 3;
 
     let strips = tear_into_strips(indices);
-    let mut indices = Vec::new();
+    let mut indices =
+      Vec::with_capacity(indices.len() + (strips.len() - 1) * 6);
 
     for strip in strips {
-      total_trigs += strip.len() - 2;
-
-      let mut begin = indices.len();
-      let mut end = begin + strip.len();
+      let begin = indices.len();
       if !indices.is_empty() {
-        // duplicate the last triangle in previous strip
-        indices.extend_from_within(indices.len() - 3..);
-        // duplicate the first triangle in next strip
-        indices.extend_from_slice(&strip[..3]);
-
-        indices.extend_from_slice(&strip);
-        begin += 3;
-        end += 3;
-      } else {
-        indices.extend_from_slice(&strip);
+        // duplicate the last vertex in previous strip
+        indices.extend_from_within(indices.len() - 1..);
+        // duplicate the first vertex in next strip
+        indices.extend_from_slice(&strip[..1]);
       }
+      indices.extend_from_slice(&strip);
 
+      let end = indices.len();
       ranges.push((begin..end, rand_color()));
-    }
-
-    // sanity check
-    if total_trigs != expected_total_trigs {
-      eprintln!(
-        "total_trigs: {}, expected_total_trigs: {}",
-        total_trigs, expected_total_trigs
-      );
-      // assert!(total_trigs == expected_total_trigs);
     }
 
     Self {
@@ -326,43 +309,53 @@ fn tear_into_strips(indices: &[u32]) -> Vec<Vec<u32>> {
     BTreeMap::new();
 
   for trig in indices.chunks(3) {
-    let mut t = [trig[0], trig[1], trig[2]];
-    t.sort();
+    let t: [u32; 3] = trig.try_into().unwrap();
     trigs.insert(t);
 
     colinear_trigs
-      .entry([t[0], t[1]])
+      .entry(sorted([t[0], t[1]]))
       .or_default()
       .push((t, t[2]));
     colinear_trigs
-      .entry([t[0], t[2]])
-      .or_default()
-      .push((t, t[1]));
-    colinear_trigs
-      .entry([t[1], t[2]])
+      .entry(sorted([t[1], t[2]]))
       .or_default()
       .push((t, t[0]));
+    colinear_trigs
+      .entry(sorted([t[2], t[0]]))
+      .or_default()
+      .push((t, t[1]));
   }
 
   let mut strips = Vec::new();
 
-  while let Some([a, mut b, mut c]) = trigs.pop_first() {
+  let next_vert = |trigs: &mut BTreeSet<_>, b, c| {
+    colinear_trigs[&sorted([b, c])]
+      .iter()
+      .filter_map(|(t, d)| trigs.contains(t).then_some((t, *d)))
+      .next()
+  };
+
+  while let Some([a, mut b, mut c]) = trigs.pop_last() {
     let mut strip = vec![a, b, c];
 
+    // heuristic for better result regardless the traversal direction
+    // of the trigs.
+    match next_vert(&mut trigs, b, c) {
+      Some(_) => {}
+      None => {
+        strip = vec![b, c, a];
+        b = c;
+        c = a;
+      }
+    }
+
     loop {
-      let mut line = [b, c];
-      line.sort();
-
-      let next_trig = colinear_trigs[&line]
-        .iter()
-        .filter_map(|(t, d)| trigs.remove(t).then_some(*d))
-        .next();
-
-      let Some(d) = next_trig else {
+      let Some((t, d)) = next_vert(&mut trigs, b, c) else {
         break;
       };
 
       // add vertex to strip and mark the triangle as processed
+      trigs.remove(t);
       strip.push(d);
 
       b = c;
@@ -430,10 +423,7 @@ fn validate_trig_strips(indices: &[u32], strips: &[Vec<u32>]) {
   let mut original_trigs: Vec<[u32; 3]> = indices
     .chunks(3)
     .map(|x| x.try_into().unwrap())
-    .map(|mut trig: [u32; 3]| {
-      trig.sort();
-      trig
-    })
+    .map(rotate3)
     .collect();
 
   original_trigs.sort();
@@ -441,15 +431,29 @@ fn validate_trig_strips(indices: &[u32], strips: &[Vec<u32>]) {
   let mut strip_trigs: Vec<[u32; 3]> = strips
     .iter()
     .flat_map(|strip| {
-      strip.windows(3).map(|x| x.try_into().unwrap()).map(
-        |mut trig: [u32; 3]| {
-          trig.sort();
-          trig
-        },
-      )
+      strip.windows(3).map(|x| x.try_into().unwrap()).map(rotate3)
     })
     .collect();
   strip_trigs.sort();
 
-  assert_eq!(original_trigs, strip_trigs);
+  // assert_eq!(original_trigs, strip_trigs);
+}
+
+fn sorted<T: Ord, const N: usize>(mut a: [T; N]) -> [T; N] {
+  a.sort();
+  a
+}
+
+// rotate the triangle vertices while maintaining the face winding
+// order
+fn rotate3<T: Ord>(mut a: [T; 3]) -> [T; 3] {
+  if a[0] > a[1] || a[0] > a[2] {
+    a.rotate_left(1);
+  }
+
+  if a[0] > a[1] || a[0] > a[2] {
+    a.rotate_left(1);
+  }
+
+  a
 }
